82ff9a0aee99278164286216b19d73fe9df46c48
Mohammed Abbas <m.abbas345.ma@gmail.com>
2022-08-21 02:30:10 +0500

    Updated with authentication conditions

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const { ensureLoggedOut, ensureLoggedIn } = require('connect-ensure-login');
 
 router.get('/', (req, res, next) => {
-  res.render('header.ejs')
-// const person = req.user;
-// console.log('person',person)
+  return res.render('header.ejs')
+  // const person = req.user;
+  // console.log('person',person)
   // res.render('index.ejs', { person });
 });
 
@@ -18,5 +18,6 @@ router.get('/', (req, res, next) => {
 // const person = req.user;
 
 // });
+
 
 module.exports = router;
